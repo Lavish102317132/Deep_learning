58fb084686ce31aab26bbde0d2d54858c31e0e88
Lavish102317132 <lgupta3_be23@thapar.edu>
2026-04-25 14:34:15 +0530

    Add files via upload

---
+++ rnn.py
@@ -0,0 +1,18 @@
+import numpy as np
+import tensorflow as tf
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import SimpleRNN, Dense
+
+X = np.random.rand(100, 10, 1)
+y = np.sum(X, axis=1)
+
+model = Sequential()
+model.add(SimpleRNN(32, input_shape=(10, 1)))
+model.add(Dense(1))
+
+model.compile(optimizer='adam', loss='mse')
+
+model.fit(X, y, epochs=10, batch_size=8)
+
+test_input = np.random.rand(1, 10, 1)
+print(model.predict(test_input))
